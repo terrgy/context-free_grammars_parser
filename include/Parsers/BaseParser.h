e0c114a65d82e1787874e9e8968c0e9a0fb9f1cb
Vlad Sidorov <mylongmind@gmail.com>
2023-11-27 16:45:56 +0300

    Add BaseParser class

---
+++ include/Parsers/BaseParser.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+#include "Grammar.h"
+
+class BaseParser {
+protected:
+    Grammar grammar;
+
+public:
+    virtual void fit(Grammar) = 0;
+    virtual bool predict(const std::string&) = 0;
+};
